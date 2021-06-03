754539074c1f2ebb675d4dd662a5c31036540ba0
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2021-06-03 11:57:26 +0200

    verify flyTo broken

--- app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.EdgeInsets
+import com.mapbox.maps.plugin.animation.flyTo
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 import com.mapbox.maps.testapp.R
 import kotlinx.android.synthetic.main.activity_simple_map.*
 
@@ -18,8 +21,18 @@ class SimpleMapActivity : AppCompatActivity() {
     mapView.getMapboxMap()
       .apply {
         setCamera(
-          CameraOptions.Builder().center(Point.fromLngLat(LONGITUDE, LATITUDE)).zoom(9.0).build()
+          CameraOptions.Builder().center(
+          Point.fromLngLat(11.557879756109912, 48.177028731678675))
+          .padding(EdgeInsets( 640.0,  960.0,  320.0,  960.0))
+          .zoom(17.5)
+          .bearing(225.0)
+          .pitch(45.0)
+          .build()
         )
+        addOnMapClickListener{
+          flyTo(CameraOptions.Builder().zoom(16.5).build())
+          true
+        }
       }
   }
 
