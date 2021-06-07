52fdb57e6c366c7b1dc5bd07dbfc91fc8c0cc3a5
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2021-06-07 20:50:28 +0200

    brb

--- app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
@@ -1,9 +1,15 @@
 package com.mapbox.maps.testapp.examples
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.extension.style.layers.properties.generated.TextAnchor
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 import com.mapbox.maps.testapp.R
 import kotlinx.android.synthetic.main.activity_simple_map.*
 
@@ -21,6 +27,23 @@ class SimpleMapActivity : AppCompatActivity() {
           CameraOptions.Builder().center(Point.fromLngLat(LONGITUDE, LATITUDE)).zoom(9.0).build()
         )
       }
+
+    mapView?.getMapboxMap()?.addOnMapClickListener() { point ->
+      val pointAnnotationPluginImpl = mapView.annotations.createPointAnnotationManager(mapView)
+      // Create the annotation
+      val annotation = pointAnnotationPluginImpl.create(
+        PointAnnotationOptions().withDraggable(false)
+        .withPoint(point)
+        .withTextAnchor(TextAnchor.BOTTOM)
+        .withTextColor(Color.BLUE)
+        .withTextSize(12.0)
+        .withTextField("Please don't crash!"))
+      Thread.sleep(1)
+      // Update its properties shortly after creation - often causes crash
+      annotation.textField = "We're an enterprise customer!"
+      annotation.textRotate = Math.random() * 180
+      true
+    }
   }
 
   override fun onStart() {
